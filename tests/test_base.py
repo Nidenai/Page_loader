d8903e739f2cb2bd0be8355385505efacef6fcc9
Nidenai <30Dk81Yz>
2021-11-17 20:11:43 +0700

    Adding Test 0.2

--- tests/test_base.py
+++ tests/test_base.py
@@ -6,16 +6,21 @@ HTML_FILE = os.path.join(os.getcwd(),
                          'tests', 'fixtures', 'ru.hexlet.io-courses.html')
 URL = 'https://ru.hexlet.io/courses'
 NAME = 'ru.hexlet.io-courses.html'
+PATH_FOR_FILE = os.path.join('tests', 'var', 'tmp')
 
+
+def clear_experiment():
+    if os.path.isfile(os.path.join(PATH_FOR_FILE, NAME)):
+        os.remove(os.path.join(PATH_FOR_FILE, NAME))
+    else:
+        pass
 
 def test_naming():
+    clear_experiment()
     test_name = os.path.basename(os.path.join(os.getcwd(),
                                               'fixtures',
                                               'ru.hexlet.io-courses.html'))
-    download(URL)
-    name = os.path.basename(os.path.join(os.getcwd(),
+    download(URL, PATH_FOR_FILE)
+    name = os.path.basename(os.path.join(PATH_FOR_FILE,
                                          'ru.hexlet.io-courses.html'))
     assert name == test_name
-
-
-def test_content():
