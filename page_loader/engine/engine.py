5bbd80df8d22f27328d8ad9ae08028a6f567b91d
Nidenai <30Dk81Yz>
2021-11-17 22:05:27 +0700

    Second step 0.1 (add creation catalog)

--- page_loader/engine/engine.py
+++ page_loader/engine/engine.py
@@ -3,17 +3,19 @@ import os
 
 
 def download(url, path_=os.getcwd()):
-    filepath = os.path.join(os.getcwd(), to_path(path_), naming_file(url))
+    filename = naming_file(url)
+    filepath = os.path.join(os.getcwd(), to_path(path_), filename)
     existing_path(to_path(path_))
     with requests.get(url, stream=True) as r:
-        with open(filepath, 'wb+') as kfile:
+        with open(filepath, 'wb+') as keyfile:
             for chunk in r.iter_content(chunk_size=128):
-                kfile.write(chunk)
-    kfile.close()
-    return kfile
+                keyfile.write(chunk)
+    keyfile.close()
+    make_catalog(filepath)
+    return keyfile
 
 
-def naming_file(file):
+def naming_file(file):  # функция нормализует имя сохраняемой страницы
     v = str(file)
     v = v.replace('https://', '')
     v = v.replace('www.', '')
@@ -21,7 +23,7 @@ def naming_file(file):
     return result
 
 
-def to_path(path_):
+def to_path(path_):  # функция нормализует введенный пользователем путь
     if path_ == '':
         return path_
     path = os.path.normpath(path_)
@@ -30,8 +32,18 @@ def to_path(path_):
     return result
 
 
-def existing_path(path):
+def existing_path(path):  # функция проверяет, существует ли путь
     if not os.path.exists(path):
         os.makedirs(path)
     elif os.path.exists(path):
         pass
+
+
+def make_catalog(name):
+    name = name.replace('.html', '') + '_files'
+    if not os.path.exists(name):
+        os.mkdir(name)
+    else:
+        pass
+
+
--- tests/test_base.py
+++ tests/test_base.py
@@ -1,27 +1,38 @@
 from page_loader.engine.engine import download
 import os
 
-
 HTML_FILE = os.path.join(os.getcwd(),
                          'tests', 'fixtures', 'ru.hexlet.io-courses.html')
 URL = 'https://ru.hexlet.io/courses'
 NAME = 'ru.hexlet.io-courses.html'
 PATH_FOR_FILE = os.path.join('tests', 'var', 'tmp')
+CATALOG_NAME = 'ru.hexlet.io-courses_files'
 
 
-def clear_experiment():
+def clear_():
     if os.path.isfile(os.path.join(PATH_FOR_FILE, NAME)):
         os.remove(os.path.join(PATH_FOR_FILE, NAME))
+    elif os.path.isdir(os.path.join(PATH_FOR_FILE, CATALOG_NAME)):
+        os.rmdir((os.path.join(PATH_FOR_FILE, CATALOG_NAME)))
     else:
         pass
 
 
 def test_naming():
-    clear_experiment()
+    clear_()
     test_name = os.path.basename(os.path.join(os.getcwd(),
                                               'fixtures',
                                               'ru.hexlet.io-courses.html'))
     download(URL, PATH_FOR_FILE)
-    name = os.path.basename(os.path.join(PATH_FOR_FILE,
-                                         'ru.hexlet.io-courses.html'))
+    name = os.path.basename(os.path.join(PATH_FOR_FILE, NAME))
     assert name == test_name
+
+
+def test_catalog():
+    clear_()
+    download(URL, PATH_FOR_FILE)
+    base_catalog = os.path.basename(os.path.join(os.getcwd(),
+                                                 'fixtures',
+                                                 'ru.hexlet.io-courses_files'))
+    catalog = os.path.basename(os.path.join(PATH_FOR_FILE, CATALOG_NAME))
+    assert base_catalog == catalog
