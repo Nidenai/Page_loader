b838578cf138399ccb4954c9ad5d3a2f22aa562f
Nidenai <30Dk81Yz>
2022-03-21 23:49:45 +0700

    fix: refactoring try-except

--- page_loader/download.py
+++ page_loader/download.py
@@ -38,23 +38,27 @@ def download_url(url, path_=os.getcwd(), filename=None):
 
 def download(url, path_=os.getcwd()):
     logger_script()
+    filename = create_filename_for_file(url)
     try:
-        filename = create_filename_for_file(url)
         download_url(url, path_, filename)
         logger.info(f'Resource by {url}] was downloaded: {filename}')
-        filepath = os.path.join(os.getcwd(), os.path.normpath(path_), filename)
-        create_html_catalog(filepath)
-        catalog = os.path.normpath(filepath).replace('.html', '_files')
-        catalog_name = os.path.basename(str(catalog))
-        downloaded_list = []
-        for item in tqdm(LIST_, desc='Getting resourses'):
-            sample = find_content(filepath, item, url)
-            downloaded_list = downloaded_list + sample
-        logger.info(downloaded_list)
-        replace_content(filepath, LIST_, url, catalog_name)
-        for link in tqdm(downloaded_list, desc='Download Files', unit=' kb'):
-            download_url(link, catalog)
-        logger.info(f"Done. You can open saved page from: {filepath}")
-        return filepath
     except Exception:
         raise TypeError('Ошибка')
+    filepath = os.path.join(os.getcwd(), os.path.normpath(path_), filename)
+    create_html_catalog(filepath)
+    catalog = os.path.normpath(filepath).replace('.html', '_files')
+    catalog_name = os.path.basename(str(catalog))
+    downloaded_list = []
+    for item in tqdm(LIST_, desc='Getting resourses'):
+        sample = find_content(filepath, item, url)
+        downloaded_list = downloaded_list + sample
+    logger.info(downloaded_list)
+    replace_content(filepath, LIST_, url, catalog_name)
+    try:
+        for link in tqdm(downloaded_list, desc='Download Files', unit=' kb'):
+            download_url(link, catalog)
+    except Exception:
+        raise TypeError('Ошибка')
+    logger.info(f"Done. You can open saved page from: {filepath}")
+    return filepath
+
