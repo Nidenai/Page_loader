cbf05579a635de8d03b7aca6bb385c811fa40f71
Nidenai <30Dk81Yz>
2022-03-29 21:16:54 +0700

    fix: add new tests

--- tests/test_base.py
+++ tests/test_base.py
@@ -5,17 +5,19 @@ import requests_mock
 from tqdm import tqdm
 
 from page_loader.download import download_url, \
-    LIST_
+    LIST_, download
 from page_loader.html import find_content
 from page_loader.url import create_filename_for_file
 
 URL = 'https://www.mirf.ru/comics/saga-komiks'
 PATH = os.path.join(os.getcwd(), 'tests', 'tmp')
 NAME = 'mirf-ru-comics-saga-komiks.html'
-FIXTURE_PATH = os.path.join(os.getcwd(), 'fixtures', 'tmp')
+FIXTURE_PATH = os.path.join(os.getcwd(), 'tests', 'fixtures')
 CATALOG_NAME = 'mirf-ru-comics-saga-komiks_files'
-FIXTURE_FIND_FILE = os.path.join(os.getcwd(), 'tests',
-                                 'fixtures', 'fixture_find.html')
+FIXTURE_FIND_FILE = os.path.join(FIXTURE_PATH, 'fixture_find.html')
+CONTENT_FIXTURE = os.path.join(FIXTURE_PATH, 'mirf-ru-comics-saga-komiks_files',
+                               'mirf-ru-wp-content-plugins-push-js-push-lib.js')
+URL_FOR_CONTENT = 'https://www.mirf.ru/wp-content/plugins/push/js/push.lib.js'
 fixture_list = ['https://docs.python-requests.org'
                 '/_static/requests-sidebar.png',
                 'https://docs.python-requests.org'
@@ -23,7 +25,7 @@ fixture_list = ['https://docs.python-requests.org'
                 'https://docs.python-requests.org'
                 '/_static/jquery.js', 'https://docs.python-req'
                                       'uests.org/_static'
-                '/underscore.js',
+                                      '/underscore.js',
                 'https://docs.python-requests.org'
                 '/_static/doctools.js',
                 'https://docs.python-requests.org'
@@ -95,3 +97,30 @@ def test_find_content():
 def test_naming():
     name = create_filename_for_file(URL)
     assert name == NAME
+
+
+def test_download():
+    ready()
+    with open(os.path.join(FIXTURE_PATH, NAME)) as f:
+        content = f.read()
+        with requests_mock.Mocker() as m:
+            m.get(URL, text=content)
+            download(URL, PATH)
+            check = os.path.isfile(os.path.join(PATH, NAME))
+            assert check is True
+            check_folder = os.path.isdir(os.path.join(PATH, CATALOG_NAME))
+            assert check_folder is True
+
+
+def test_resourse():
+    with open(CONTENT_FIXTURE) as f:
+        content = f.read()
+        with requests_mock.Mocker() as m:
+            m.get(URL_FOR_CONTENT, text=content)
+            download_url(URL_FOR_CONTENT, PATH)
+            with \
+                    open(os.path.join(PATH,
+                                      'mirf-ru-wp-content-plugins-push'
+                                      '-js-push-lib.js')) as q:
+                compare = q.read()
+                assert compare == content
